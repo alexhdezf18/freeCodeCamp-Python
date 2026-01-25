3b0bbc219ae951c121b70920ff5d47c9f440c95f
Alex Hernandez <alexhdezf18@gmail.com>
2026-01-24 21:29:30 -0600

    Build a Player Interface Lab

---
+++ Player-Interface/mian.py
@@ -0,0 +1,29 @@
+import random
+from abc import ABC, abstractmethod
+
+class Player(ABC):
+    def __init__(self):
+        self.moves = []
+        self.position = (0, 0)
+        self.path = [self.position]
+    
+    def make_move(self):
+        random_move = random.choice(self.moves)
+        new_x = self.position[0] + random_move[0]
+        new_y = self.position[1] + random_move[1]
+        self.position = (new_x, new_y)
+        self.path.append(self.position)
+        return self.position
+    
+    @abstractmethod
+    def level_up(self):
+        pass
+
+class Pawn(Player):
+    def __init__(self):
+        super().__init__()
+        self.moves = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+    
+    def level_up(self):
+        diagonal_moves = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
+        self.moves.extend(diagonal_moves)
